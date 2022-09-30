967646d325aa2a8cf4dcbbeecd99cfef9a840538
zota <harshitrawat881@gmail.com>
2022-09-30 22:07:42 +0530

    updated footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -3,27 +3,33 @@ import Link from "next/link";
 
 export default function Footer(){
     return (
-        <div className={footer.footer}>
-            <div className={footer.content}>
-                <h3>C.I.K.Y.</h3>
-                <Link href="#" ><a>Home</a></Link>
-                <Link href="#" ><a>Clubs</a></Link>
-                <Link href="#" ><a>Organize</a></Link>
-                <Link href="#" ><a>Compete</a></Link>
-                <Link href="#" ><a>Privacy Policy</a></Link>
-                <Link href="#" ><a>Feedback</a></Link>
-            </div>  
-            <div className={footer.content}>
-                <h3>About Us</h3>
-                <Link href="#" ><a>Team Section</a></Link>
-                <Link href="#" ><a>Technology </a></Link>
-                <Link href="#" ><a>Our Other Projects</a></Link>
-            </div>
-            <div className={footer.content}>
-                <h3>Resources</h3>
-                <Link href="#" ><a></a></Link>
-                <Link href="#" ><a>Technology </a></Link>
-                <Link href="#" ><a>Our Other Projects</a></Link>
+        <div className={footer.container}>
+            <div className={footer.footer}>
+                <div className={footer.suggestion_div}>
+                    <h3 className={footer.head}>Have suggestions?</h3>
+                    <textarea rows='4' className={footer.suggestion}>jj</textarea>
+                    <button className={footer.button}>Submit</button>
+                </div>
+                <hr />
+                <div className={footer.navigation}>
+                    <div className={footer.navlist}>
+                        <h3>C.I.K.Y.</h3>
+                        <Link href="#" ><a>Home</a></Link>
+                        <Link href="#" ><a>Clubs</a></Link>
+                        <Link href="#" ><a>Organize</a></Link>
+                        <Link href="#" ><a>Compete</a></Link>
+                        <Link href="#" ><a>Feedback</a></Link>
+                    </div>  
+                    <hr />
+                    <div className={footer.navlist}>
+                        <h3>About Us</h3>
+                        <Link href="#" ><a>Team Section</a></Link>
+                        <Link href="#" ><a>Technology </a></Link>
+                        <Link href="#" ><a>Our Other Projects</a></Link>
+                    </div>
+                </div>
+                <hr />
+                
             </div>
         </div>
     )
