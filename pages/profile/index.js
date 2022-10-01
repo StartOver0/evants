0483be378b1855868407f96155dcd48f5f2167da
zota <harshitrawat881@gmail.com>
2022-10-01 18:40:57 +0530

    testing the profile page

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,8 +1,9 @@
-import Avatar from "../../components/Avatar/avatar";
+import Avatar from "/components/Avatar/avatar";
 import { data } from "/staticData/profiledata.js";
 const subheadings = ["upcoming events", "past events"];
 import { articles } from "/staticData/article.js";
 import BlogPreview from "../../components/blogPreview/blogPreview";
+
 export default function Home() {
   return (
     <div>
