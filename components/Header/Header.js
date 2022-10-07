43fbc3852ac9022ddfda625610210573dad28f95
pradeep <pradeep8b0@gmail.com>
2022-10-07 14:40:17 +0530

    new footer

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -3,12 +3,13 @@ import Image from "next/image";
 import icon from "/public/favicon.ico";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import { UserContext } from "../../lib/Context";
 import { auth, db } from "../../lib/firebase";
 import { signOut } from "firebase/auth";
 import { getDoc, doc } from "firebase/firestore";
 import toast from "react-hot-toast";
+
 export default function Header() {
   const router = useRouter();
   const [profileImg, setProfileImg] = useState(null);
@@ -33,27 +34,33 @@ export default function Header() {
     );
   };
 
-  if (username && user) {
-    (async () => {
-      let Doc = doc(db, "users", user.uid);
-      let snapshot = await getDoc(Doc);
-
-      setProfileImg(snapshot.data().photoURL);
-    })();
-  }
+  useEffect(() => {
+    if (username && user) {
+      (async () => {
+        let Doc = doc(db, "users", user.uid);
+        let snapshot = await getDoc(Doc);
+        setProfileImg(snapshot.data().photoURL);
+      })();
+    }
+  });
 
   return (
     <div className={nav.navbar}>
-      <div className={nav.domain_name}>
-        <h1>C . I . K . Y </h1>
-      </div>
-      <div className={nav.domain_icon}>
-        <div className={nav.icon_container}>
-          <Image src={icon} alt="A Logo" width={40} height={40} />
-        </div>
-      </div>
+      <Link href="/">
+        <a>
+          <div className={nav.domain_name}>
+            <h1>C . I . K . Y </h1>
+          </div>
+          <div className={nav.domain_icon}>
+            <div className={nav.icon_container}>
+              <Image src={icon} alt="A Logo" width={40} height={40} />
+            </div>
+          </div>
+        </a>
+      </Link>
+
       {user && username && profileImg ? (
-        <div className="flex w-[200px] justify-around">
+        <div className="flex w-[225px] mr-4 justify-around">
           <button
             onClick={async () => {
               try {
@@ -64,7 +71,7 @@ export default function Header() {
                 toast.error(err.message.toString());
               }
             }}
-            className="hover:bg-red-600 ml-6 border-2 hover:text-white border-blue-800 border-solid leading-[2px]  px-[30px]"
+            className="hover:bg-red-600 ml-6 border-2 hover:text-white border-red-700 border-solid leading-[2px] px-[30px]"
           >
             Log out
           </button>
@@ -88,7 +95,9 @@ export default function Header() {
             </button>
           </div>
           <div className={nav.navlinks_mobile}>
-            <button className={nav.signUpButton}>Login/Sign Up</button>
+            <button className={nav.signUpButton} onClick={gotoLogin}>
+              Login/Sign Up
+            </button>
           </div>
         </>
       )}
