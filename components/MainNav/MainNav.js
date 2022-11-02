e78c835393dd165104fe0bbdc03d8f7476807dfe
pradeep <pradeep8b0@gmail.com>
2022-11-02 15:53:59 +0530

    something

--- components/MainNav/MainNav.js
+++ components/MainNav/MainNav.js
@@ -1,60 +1,51 @@
-import nav from './MainNav.module.css';
-import Link from 'next/link';
-import Image from 'next/image';
+import nav from "./MainNav.module.css";
+import Link from "next/link";
+import Image from "next/image";
 import homeIcon from "/public/nee.png";
-import {useEffect, useState} from 'react';
-import { useRouter } from 'next/router';
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
-export default function MainNav(){
-
+export default function MainNav() {
   const [chosen, setChosen] = useState("");
   const Router = useRouter();
 
   useEffect(() => {
-      const splitPath = Router.pathname.split('/');
-      setChosen(splitPath[1]);
-  }, [Router])
-  
-  return(
+    const splitPath = Router.pathname.split("/");
+    setChosen(splitPath[1]);
+  }, [Router]);
+
+  return (
     <div className={nav.mainNav}>
-      
-          <Link href="/" shallow={true}>
-            <div className={nav.cover} >
-                <div className={nav.homeLink}>
-                    <a className={chosen == '' ? nav.active: ''}>Home</a>
-                </div>
-                <div className={nav.homeIcon}>
-                    <a className={chosen == '' ? nav.active: ''}><Image src={homeIcon} width={20} height={20} alt="icon"/></a>
-                </div>
-            </div>
-          </Link>
-      
-      
-        <Link href="/organize">
-          <div className={nav.cover} >
-            <a className={chosen == 'organize' ? nav.active: ''}>Organize</a>
+      <Link href="/" shallow={true}>
+        <div className={nav.cover}>
+          <div className={nav.homeLink}>
+            <a className={chosen == "" ? nav.active : ""}>Home</a>
           </div>
-        </Link>
-      
-      
-      <Link href="/events">
-          <div className={nav.cover} >
-              <a className={chosen == 'events' ? nav.active: ''}>Events</a>
+          <div className={nav.homeIcon}>
+            <a className={chosen == "" ? nav.active : ""}>
+              <Image src={homeIcon} width={20} height={20} alt="icon" />
+            </a>
           </div>
+        </div>
       </Link>
 
-      <Link  href="/clubs">
-          <div className={nav.cover} >
-              <a className={chosen == 'clubs' ? nav.active: ''} >Clubs</a>
-          </div>
-      </Link>
-      
-      <Link  href="/about">
-          <div className={nav.cover} >
-              <a className={chosen == 'about' ? nav.active: ''}>About Us</a>
-          </div>
+      <Link href="/organize">
+        <div className={nav.cover}>
+          <a className={chosen == "organize" ? nav.active : ""}>Organize</a>
+        </div>
       </Link>
 
+      <Link href="/clubs">
+        <div className={nav.cover}>
+          <a className={chosen == "clubs" ? nav.active : ""}>Clubs</a>
+        </div>
+      </Link>
+
+      <Link href="/about">
+        <div className={nav.cover}>
+          <a className={chosen == "about" ? nav.active : ""}>About Us</a>
+        </div>
+      </Link>
     </div>
   );
 }
