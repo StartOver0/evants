0354626017f61e8a968d798de215728531504369
pradeep <pradeep8b0@gmail.com>
2023-07-28 23:20:31 +0530

    >>

--- staticData/teamData.js
+++ staticData/teamData.js
@@ -1,24 +1,24 @@
-import teamMember1 from '/public/images/mdl.jpg';
-import teamMember2 from '/public/images/ichi.jpg';
-import teamMember3 from '/public/images/nar.jpg';
+import teamMember1 from "/public/images/mdl.jpg";
+import teamMember2 from "/public/images/ichi.jpg";
+import teamMember3 from "/public/images/nar.jpg";
 
-module.exports= [
-    {   
-        name: "Harshit Rawat",
-        work: "Web Developer",
-        img: teamMember1,
-        instagram: "https://instagram.com",
-        twitter: "https://twitter.com",
-        linkedin: "https://linkedin.com",
-        facebook: "https://facebook.com",
-    },
-    {   
-        name: "Pradeep Bisht",
-        work: "Pro Web Developer",
-        img: teamMember2,
-        instagram: "https://instagram.com",
-        twitter: "https://twitter.com",
-        linkedin: "https://linkedin.com",
-        facebook: "https://facebook.com",
-    },
-]
+module.exports = [
+  {
+    name: "Pradeep Bisht",
+    work: "Pro Web Developer",
+    img: teamMember2,
+    instagram: "https://instagram.com",
+    twitter: "https://twitter.com",
+    linkedin: "https://linkedin.com",
+    facebook: "https://facebook.com",
+  },
+  {
+    name: "Harshit Rawat",
+    work: "Web Developer",
+    img: teamMember1,
+    instagram: "https://instagram.com",
+    twitter: "https://twitter.com",
+    linkedin: "https://linkedin.com",
+    facebook: "https://facebook.com",
+  },
+];
